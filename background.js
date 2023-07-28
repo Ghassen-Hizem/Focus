c87613219f8971de963e374cd1317d3d0722c9cc
hizem <ghassen.hizem@supcom.tn>
2023-07-28 13:56:08 +0100

    bug fixed

--- background.js
+++ background.js
@@ -16,3 +16,31 @@ window.addEventListener("storage", () => {
     }
   })
 
+  let updating;
+
+  function onUpdated(tab) {
+    console.log(`Updated tab: ${tab.id}`);
+  }
+  
+  function onError(error) {
+    console.log(`Error: ${error}`);
+  }
+  
+ function handleCreated(tabID,changeinfo,tab) {
+   // console.log(tab.url);
+   if((tab.url == "https://www.facebook.com/") || (tab.url == "https://www.instagram.com/")){
+    if((tab.status=="complete") && (localStorage.getItem("State")=="Flow")){
+        // window.alert("The Focus State is activated , You will be redirected to another page")
+        let updating = browser.tabs.update(tabID,{
+            url: "https://google.com"
+        });
+        updating.then(onUpdated,onError);
+    }
+       
+        
+   }
+  }
+
+
+ browser.tabs.onUpdated.addListener(handleCreated);
+
